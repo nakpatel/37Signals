f71823cf48a7d86abcde647b6999593f75f2cd1a
Shonak Patel <patel.shonak@gmail.com>
2013-06-30 20:58:41 -0400

    box positioning and other css fixes
    Still need to fix the bc positioning

--- application.js
+++ application.js
@@ -7,42 +7,42 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 
 
 $(document).ready(function(){
-	$('.box-bc').on('mouseenter', function(){
+		$('.box-bc').on('mouseenter', function(){
 		event.preventDefault();
 		$('.banner').hide();
 		$(this).find('.boxarrowleft').show();
 		$('.bc-banner').show();
 	});	
-
-	$('.box-bc').on('mouseleave', function(){
+	
+		$('.box-bc').on('mouseleave', function(){
 		event.preventDefault();
 		$(this).find('.boxarrowleft').hide();
 		$('.bc-banner').hide();
 		$('.banner').show();
 	});	
 
-	$('.box-hr').on('mouseenter', function(){
+		$('.box-hr').on('mouseenter', function(){
 		event.preventDefault();
 		$('.banner').hide();
 		$(this).find('.boxarrowleft').show();
 		$('.hr-banner').show();
 	});	
 	
-	$('.box-hr').on('mouseleave', function(){
+		$('.box-hr').on('mouseleave', function(){
 		event.preventDefault();
 		$(this).find('.boxarrowleft').hide();
 		$('.hr-banner').hide();
 		$('.banner').show();
 	});	
 
-	$('.box-cf').on('mouseenter', function(){
+		$('.box-cf').on('mouseenter', function(){
 		event.preventDefault();
 		$('.banner').hide();
 		$(this).find('.boxarrowright').show();
 		$('.cf-banner').show();
 	});	
 	
-	$('.box-cf').on('mouseleave', function(){
+		$('.box-cf').on('mouseleave', function(){
 		event.preventDefault();
 		$(this).find('.boxarrowright').hide();
 		$('.cf-banner').hide();
